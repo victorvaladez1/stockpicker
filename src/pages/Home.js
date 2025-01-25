0f1fcf17308eb3836fa5c9fa4fe8dc552f59e6de
Victor Valadez <vvaladez315@gmail.com>
2025-01-25 14:58:08 -0600

    Created Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: "center", padding: "20px"}}>
+            <p>Stock Picker</p>
+        </div>
+    );
+};
+
+export default Home;
