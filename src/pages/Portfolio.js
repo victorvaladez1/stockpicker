b8edd6b4841747ccb107318d110791001f786961
Victor Valadez <vvaladez315@gmail.com>
2025-01-25 21:13:02 -0600

    Created hero component for porfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,24 +1,13 @@
 import React from "react";
 import Navbar from "../components/NavBar/Navbar";
 import Footer from "../components/Footer/Footer";
+import PortfolioHero from "../components/PortfolioHero/PortfolioHero.js";
 
 const Portfolio = () => {
     return (
         <div>
             <Navbar />
-            <div style={{ padding: "20px", textAlign: "center" }}>
-                <h1>My Portfolio</h1>
-                <p>Welcome to the Portfolio page! Here you'll find information about your stock portfolio.</p>
-                {/* Add portfolio details below */}
-                <div>
-                    <h2>Stocks Overview</h2>
-                    <ul>
-                        <li>Stock A: 10 shares at $100</li>
-                        <li>Stock B: 5 shares at $50</li>
-                        <li>Stock C: 20 shares at $200</li>
-                    </ul>
-                </div>
-            </div>
+            <PortfolioHero />
             <Footer />
         </div>
     );
