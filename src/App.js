deaa9b331385e897b73c19d6939ea614d04106cb
Victor Valadez <vvaladez315@gmail.com>
2025-01-25 19:21:12 -0600

    Created Portfolio page and routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js";
-import GetStarted from "./pages/GetStarted.js";
+import Home from "./pages/Home";
+import GetStarted from "./pages/GetStarted";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} /> {}
-        <Route path="/get-started" element={<GetStarted />} /> {}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/get-started" element={<GetStarted />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -7,16 +7,13 @@ const NavBar = () => {
         <nav className="navBar">
             <ul className="navList">
                 <li className="navItem">
-                    <Link to="/" className="navLink">Home</Link> {/* Redirects to Home */}
+                    <Link to="/" className="navLink">Home</Link> {/* Proper React Router Link */}
                 </li>
                 <li className="navItem">
-                    <a href="#analysis" className="navLink">Analysis</a>
+                    <Link to="/get-started" className="navLink">Get Started</Link> {/* Proper React Router Link */}
                 </li>
                 <li className="navItem">
-                    <a href="#marketnews" className="navLink">Market News</a>
-                </li>
-                <li className="navItem">
-                    <a href="#portfolio" className="navLink">Portfolio</a>
+                    <Link to="/portfolio" className="navLink">Portfolio</Link> {/* Proper React Router Link */}
                 </li>
             </ul>
         </nav>
